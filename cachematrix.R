17ef4afaed572dac55b368422d0c31e3d45728a2
AnneHeidi <anneheidi@gmail.com>
2015-04-23 12:46:00 +0200

    compute inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+##### Assignment 2 - Lexical Scoping and caching
+####################
 
-## Write a short comment describing this function
+## Cache: a Place to store something temporarily in a computing environment
+
+## overall description of what the functions do: 
+    # MakeCacheMatrix makes a list containing a function to set the value of a matrix (x), get the value of the matrix
+    # and to Set the value of the inversematrix (inverseM) and get the value of the inversematrix 
 
 makeCacheMatrix <- function(x = matrix()) {
 
